9143116b85db84f92cc06ea920a25486973abf7b
itslukeDev <82327125+itslukeDev@users.noreply.github.com>
2025-09-29 20:59:23 +0800

    feat: add basic search bar ui

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="flex bg-grey-900 h-dvh text-white flex-col">
     <header class="bg-grey-800 p-[calc(1.25*1em)]"><branding-logo /></header>
     <main class="flex flex-col my-8 gap-6 px-[calc(1.25*1em)]">
+      <weather-search />
       <selected-location />
       <favorite-locations />
     </main>
--- src/presentation/components/WeatherSearch.vue
+++ src/presentation/components/WeatherSearch.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="p-4 grid gap-4"></div>
+  <div class="flex flex-row">
+    <input
+      type="text"
+      placeholder="Search City"
+      class="bg-white py-1 px-3 text-black flex w-[370px] -outline-offset-4 rounded-l-lg"
+    />
+    <button class="h-full px-3 bg-grey-700 rounded-r-lg">Search</button>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
