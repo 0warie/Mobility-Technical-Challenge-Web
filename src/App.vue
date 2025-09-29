bf0d9e5ab15c7f915a202f40ce8c9c3d0a73cdcf
itslukeDev <82327125+itslukeDev@users.noreply.github.com>
2025-09-29 20:45:00 +0800

    feat: give header a colour

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex bg-gray-900 h-dvh text-white flex-col p-[calc(1.25*1em)]">
-    <header><branding-logo /></header>
-    <main class="flex flex-col my-8 gap-6">
+  <div class="flex bg-grey-900 h-dvh text-white flex-col">
+    <header class="bg-grey-800 p-[calc(1.25*1em)]"><branding-logo /></header>
+    <main class="flex flex-col my-8 gap-6 px-[calc(1.25*1em)]">
       <selected-location />
       <favorite-locations />
     </main>
